3a7b5d2da1d0ded765313de26b8b9aa53c230414
Yacine Rezgui <rezgui.y@gmail.com>
2015-05-13 01:25:11 +0100

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  externals: {
+    // 'parse-sdk': 'Parse'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
+      loader: 'babel' // The module to load. "babel" is short for "babel-loader"
+    }]
+  }
+};
